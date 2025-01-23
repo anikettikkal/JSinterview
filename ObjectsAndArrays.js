4178e4c03c949c67e13a1d2167f9ceba00f962a4
anikettikkal <tikkalaniket2019@gmail.com>
2025-01-23 20:35:56 +0530

    objects and arrays file created also starting examples and interview question on it

---
+++ ObjectsAndArrays.js
@@ -0,0 +1,15 @@
+/*1.How can you create objects in JavaScript?
+    in curly braces we are creating a object{}
+    by using new keyword we are creating a objects
+ */
+const anyObj = new Object()
+anyObj.name= "aniket tikkal"
+anyObj.mail= 'tikkalaniket03@gmail.com'
+console.log(anyObj);
+
+const another = {
+    nametwo:"banti sheth",
+    mailId:"bantisheth3@gmail.com"
+}
+console.log(another);
+
